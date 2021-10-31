42b1ee946c6ea7fc21bdc0391801b946d47d10fb
shehroze-1122 <stalat.bee19seecs@seecs.edu.pk>
2021-10-31 14:48:30 +0500

    fixing layout issues

--- src/Posts/Posts.tsx
+++ src/Posts/Posts.tsx
@@ -13,10 +13,10 @@ const Posts: React.FC<prop> = ({setCurrentId}) => {
     const data = useSelector((state: postsStateType)=>state.posts);
     return (
         data.length?(
-        <div style={{marginTop:'10px'}}>
-            <Grid container spacing={3}>
+        <div style={{marginTop:'10px', marginBottom: '50px'}}>
+            <Grid container spacing={3} >
                 {data.map((post)=>(
-                    <Grid item xs={12} sm={6} alignItems='stretch' key={String(post._id)}>
+                    <Grid item xs={12} sm={6} alignItems='stretch' justifyContent='center' key={String(post._id)}>
                             <Post
                             post={post}
                             setCurrentId={setCurrentId}
